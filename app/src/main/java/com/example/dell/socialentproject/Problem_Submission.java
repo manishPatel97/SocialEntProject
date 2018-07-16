b70e48816e6a7373580e22e1923988ae623142b5
Manish patel <manish13058@iiitd.ac.in>
2018-07-16 14:20:10 +0530

    problem submission

--- app/src/main/java/com/example/dell/socialentproject/Problem_Submission.java
+++ app/src/main/java/com/example/dell/socialentproject/Problem_Submission.java
@@ -2,6 +2,8 @@ package com.example.dell.socialentproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class Problem_Submission extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Problem_Submission extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem__submission);
+    }
+    public void login(View view){
+        Intent i =  new Intent(this,LoginActivity.class);
+        startActivity(i);
+    }
+    public  void userHome(View view){
+        Intent i = new Intent(this,userHome.class);
+        startActivity(i);
     }
 }
